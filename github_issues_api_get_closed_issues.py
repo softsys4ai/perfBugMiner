91fa586215d622efd44e1b7005060c01a633ad3b
YANG REN <38876171+kevinren12138@users.noreply.github.com>
2018-06-08 08:24:43 -0400

    Update github_issues_api_get_closed_issues.py

--- github_issues_api_get_closed_issues.py
+++ github_issues_api_get_closed_issues.py
@@ -34,7 +34,10 @@ def SaveHeader():
     h=[]
     h.append("ISSUE TITLE")
     h.append("ISSUE`S LABEL")
-    h.append("ISSUE`S TIMELINE")
+    h.append("ISSUE`S CREATED TIME")
+    h.append("ISSUE`S UPDATED TIME")
+    h.append("ISSUE`S CLOSED TIME")
+    h.append("ISSUE DESCRIPTION")
     h.append("CHARACTER OF THE USER WHO CLOSED THE ISSUE")
     h.append("COMMENT OF THE USER WHO CLOSED THE ISSUE")
     for i in range(1,40):
@@ -69,7 +72,7 @@ while True:
         labels = json.dumps(item['labels'])
         labels = labels.encode("utf-8")
         body = item['body'].encode('utf-8')
-        if "type:bug/performance" not in labels.lower() and "performance" not in title.lower() and "performance" not in body.lower():
+        if "type:bug/performance" not in labels.lower() and  keyword not in title.lower() and keyword not in body.lower():
             continue
         if title in titles:
             continue
@@ -82,10 +85,19 @@ while True:
                     closed_commemt = temp['body'].encode("utf-8")
             if len(comments) > MaxCommentNum:
                 MaxCommentNum = len(comments)
-        timeline = "created_at:"+item['created_at']+",updated_at:"+item['updated_at']+",closed_at:"+item['closed_at']+",author_association:"+item['author_association']
+        CREATED_DATE = item['created_at']
+        UPDATED_DATE = item['updated_at']
+        CLOSED_DATE = item['closed_at']
+        label_name = []
+        for label in item['labels']:
+            label_name.append(label['name'])
+        labels_name = ",".join(label_name)
         it.append(title)
-        it.append(labels)
-        it.append(timeline)
+        it.append(labels_name)
+        it.append(CREATED_DATE)
+        it.append(UPDATED_DATE)
+        it.append(CLOSED_DATE)
+        it.append(body)
         it.append(closed_user)
         it.append(closed_commemt)
         for i in comments:

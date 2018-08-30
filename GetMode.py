42ecd6cc7cf78e0405d9b5b008d20be87d797ff8
YANG REN <38876171+kevinren12138@users.noreply.github.com>
2018-08-30 10:22:43 -0400

    Update GetMode.py

--- GetMode.py
+++ GetMode.py
@@ -10,13 +10,7 @@ from sklearn import svm
 from nltk.corpus import stopwords
 from collections import Counter
 from nltk.stem.porter import *
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import feature_extraction
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.tree import DecisionTreeRegressor  
-from sklearn.ensemble import RandomForestRegressor  
-from sklearn.naive_bayes import GaussianNB 
+from Network import *
 from config import *
 import sys
 reload(sys)
@@ -31,6 +25,7 @@ trainx = []
 tariny = []
 label = []
 
+keywordslist = []
 
 with open("traindata") as f:
     reader = csv.reader(f)
@@ -79,23 +74,32 @@ def get_feature_word(teams):
 	for i in d:
 		if d[i]>=200:# and d[i]<=3000:
 			m.append(i)
+		if d[i]>40:
+			keywordslist.append(i)
 	return m
 
+
+
+
+
 def getX(teams):
-	dict_keyword = get_feature_word(all_word)
-	global kwords
-	kwords = kwords+dict_keyword
+	# if dict_keyword is []:
+	# dict_keyword = get_feature_word(all_word)
 	# print len(dict_keyword)
 	# print type(dict_keyword[0])
 	X=[]
 	for team in teams:
 		x=[0]*len(dict_keyword)
 		# print team
+		armm = [0]*len(keywordslist)
 		for i in team:
 			if i in dict_keyword:
 				x[dict_keyword.index(i)]=1
+			if i in keywordslist:
+				armm[keywordslist.index(i)]=1
 		# print x
 		X.append(x)
+		armingmaxtrix.append(armm)
 		# exit()
 	X=np.array(X)
 	# print X.shape
@@ -103,15 +107,32 @@ def getX(teams):
 
 
 
+def word2x(word):
+	x = [0]*len(dict_keyword)
+	for w in word:
+		if w in dict_keyword:
+			x[dict_keyword.index(w)]=1
+	x = np.array(x)
+	return x
+
+
+def word2arm(word):
+	x = [0]*len(keywordslist)
+	for w in word:
+		if w in keywordslist:
+			x[keywordslist.index(w)] = 1
+	# x = np.array(x)
+	return x
+
 
 def getmode(X,Y):
-	# clf = RandomForestRegressor()
-	# clf.fit(X,Y.ravel())
-	#svm
-	# clf = svm.SVC()
-	# clf.fit(X,Y.ravel())
 	clf = GaussianNB().fit(X,Y.ravel())
 	return clf
+
+# dict_keyword = get_feature_word(all_word)
+keywordslist += kwords
+armingmaxtrix = []
+
 
 modelabels = []
 for l in label:
@@ -121,34 +142,62 @@ for l in label:
 
 Y = []
 for l in label:
-	Y.append(modelabels.index(l))
+	y =[0]*len(modelabels)
+	y[modelabels.index(l)] = 1
+	Y.append(y)
 Y = np.array(Y)
 # print Y.shape
+
 
 teams = traindata
 teams = [setFeature(i) for i in teams]
 
+dict_keyword = get_feature_word(all_word)
 
-# test_list = [setFeature(i) for i in test_list1]
-train_X = getX(teams)
+train_x = getX(teams)
 
-# print train_X.shape
-mode = getmode(train_X,Y)
+# print train_x.shape
+# print Y
+# print Y.shape
+  
+mode = Network([train_x.shape[1],40,30,Y.shape[1]])
+# a.put()
+mode.fit(train_x,Y)
+# res = mode.predict(train_x[10])
+# print res.tolist().index(max(res.tolist()))
+# print Y[10].tolist().index(max(Y[10].tolist()))
+# print armingmaxtrix
+# print keywordslist
+# print len(keywordslist)
+# print len(armingmaxtrix)
+# print len(armingmaxtrix[0])
+
+chapter_num = len(armingmaxtrix)
+wordmatrix = [[0 for _ in keywordslist] for _ in keywordslist]
+# print len(wordmatrix)
+# print len(wordmatrix[0])
+
+for _rowindex in range(0,chapter_num):
+	for _firstindex in range(0,chapter_num):
+		for _secondindex in range(_firstindex,chapter_num):
+			if armingmaxtrix[_rowindex][_firstindex] is 0:
+				continue
+			if armingmaxtrix[_rowindex][_secondindex]:
+				wordmatrix[_firstindex][_secondindex] += 1
+				wordmatrix[_secondindex][_firstindex] += 1
+
+# print max(armingmaxtrix)
 
 
-def acc(mode,test_X,test_Y):
-	_Y = mode.predict(test_X)
-	_Y = np.array([_Y])
-	_y = np.transpose(_Y)
-	res = (test_Y==_y)
-	return float(np.sum(res==True))/20000.0
+
+
+
+
+# mode = getmode(train_X,Y)
+
 
 
 
 # if __name__ == "__main__":
 # 	mode = getmode()
-# 	# print acc(mode,test_X,test_Y)
-
-
-
-# predict_y = mode.predict_y
+ # 	# print acc(mode,test_X,test_Y)
